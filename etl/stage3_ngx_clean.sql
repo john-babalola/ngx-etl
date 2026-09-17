62fa7fe16e1a3b447d38bd3f6e8e39dd3457805f
John Babalola <irejohn20@gmail.com>
2026-09-17 11:00:36 +0100

    Fix stage3 SQL to match actual NGX schema (asi_value, polled_at, price_last_updated)

--- etl/stage3_ngx_clean.sql
+++ etl/stage3_ngx_clean.sql
@@ -2,24 +2,26 @@ CREATE OR REPLACE TABLE `{PROJECT}.{NGX_CLEAN}` AS
 WITH ranked AS (
   SELECT
     *,
-    DATETIME(TIMESTAMP(price_last_updated), "Africa/Lagos") AS updated_wat,
-    -- bucket to the containing 20-min interval, aligned to 09:00
+    -- bucket to the containing 20-min interval, aligned to 09:00 WAT
     TIMESTAMP_SECONDS(
-      DIV(UNIX_SECONDS(TIMESTAMP(price_last_updated)), 1200) * 1200
+      DIV(UNIX_SECONDS(price_last_updated), 1200) * 1200
     ) AS interval_bucket,
     ROW_NUMBER() OVER (
       PARTITION BY price_last_updated
-      ORDER BY retrieved_at ASC
+      ORDER BY polled_at ASC
     ) AS rn
   FROM `{PROJECT}.{NGX_RAW_STAGING}`
 )
 SELECT
   interval_bucket,
   DATE(DATETIME(interval_bucket, "Africa/Lagos")) AS trading_day,
-  current_value,
+  asi_value AS current_value,
+  prev_close,
+  price_change,
+  price_change_pct,
   price_last_updated,
-  -- staleness flag: did this reading actually refresh vs. the prior retrieval?
-  LAG(current_value) OVER (ORDER BY interval_bucket) = current_value AS is_stale
+  polled_at,
+  LAG(asi_value) OVER (ORDER BY interval_bucket) = asi_value AS is_stale
 FROM ranked
 WHERE rn = 1
 ORDER BY interval_bucket;
